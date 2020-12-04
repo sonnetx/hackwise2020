3e05f320c1cf142e3a4b3a62934210b1cd6f04ae
Vladyslav Nekriach <ukitta555@gmail.com>
2020-12-04 19:07:05 -0200

    added buttons and some handlers

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,33 @@ import React from 'react'
 import '../App.css'
 
 const Header = () => {
+  // different buttons should be visible in header
+  // when we are logged in and when we aren't
+
+  // use axios library to send request to server
+  // use json-server library to simulate having data on the server
+  const handleLoginClick = () => {
+    console.log('this button handles logging in')
+  }
+  // more handlers to add...
   return (
     <div id = 'Header'>
       this is header
+      <button
+      onClick = {() => console.log('...')}
+      >
+        sign up
+      </button>
+      <button
+      onClick = {handleLoginClick}
+      >
+        log in
+      </button>
+      <button
+      onClick = {() => console.log('...')}
+      >
+        log out
+      </button>
     </div>
   )
 }
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
 const MainMenu = () => {
+  // main screen in file I sent you before === action screen in console logs
+  const handleCatClick = () => {
+    console.log('this button is reponsible for changing the action screen to cat, options menu offers you to feed the cat')
+  }
+
+  const handleDiaryClick = () => {
+    console.log('this button is reponsible for changing action screen to text box, where person will write notes, options menu offers to save note or view previous ones')
+  }
+
+  const handleHelpClick= () => {
+    console.log('this button is responsible for showing help on action screen, option menu is empty at that point')
+  }
+
   return (
     <div id = 'MainMenu'>
       this is main menu
+      <button
+      onClick = {handleCatClick}
+      >
+        Cat's room
+      </button>
+      <button
+      onClick = {handleDiaryClick}
+      >
+        Write something in a diary
+      </button>
+      <button
+      onClick = {handleHelpClick}
+      >
+        Help
+      </button>
+
     </div>
   )
 }
